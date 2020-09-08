9fb3deb8705d1542d9637efc4d86f9c4f131e0c5
pradeepmu-kore <muniganti.pradeep@kore.com>
2020-09-08 22:18:58 +0530

    Trailts Functional modal add traits & tables list

--- findly-builder/src/app/components/traits/traits.component.ts
+++ findly-builder/src/app/components/traits/traits.component.ts
@@ -10,6 +10,11 @@ import { KRModalComponent } from 'src/app/shared/kr-modal/kr-modal.component';
 export class TraitsComponent implements OnInit {
   @ViewChild('statusModalPop') statusModalPop: KRModalComponent;
   statusModalPopRef: any = [];
+  traitsObj: any = [];
+  traitType: string;
+  traitsTableData: any = [];
+  loaderFlag: boolean = false;
+  emptyData: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
@@ -20,6 +25,45 @@ export class TraitsComponent implements OnInit {
   closeStatusModal() {
     if (this.statusModalPopRef &&  this.statusModalPopRef.close) {
       this.statusModalPopRef.close();
+    }
+   }
+   addTraits(traitValue) {
+     if(traitValue.value) {
+      // console.log(traitValue.value);
+      let obj = {
+        traits: traitValue.value,
+        utterances: []
+      }
+      this.traitsObj.push(obj);
+      traitValue.value = '';
+     }
+   }
+   addUtterances(utteranceValue, parentIndex) {
+     if(utteranceValue.value) {
+      // console.log(utteranceValue.value);
+      this.traitsObj[parentIndex].utterances.push(utteranceValue.value);
+      utteranceValue.value = '';
+     }
+  }
+   removeTraits(index) {
+    this.traitsObj.splice(index, 1);
+   }
+   removeUtterances(index, parentIndex) {
+    this.traitsObj[parentIndex].utterances.splice(index, 1);
+   }
+   saveTraitsData() {
+    //  this.traitsTableData
+    if(this.traitsObj && this.traitsObj.length) {
+      let mockData = {
+        traitType: this.traitType,
+        traits: this.traitsObj
+      };
+      this.traitsTableData.push(mockData);
+      // console.log(this.traitsObj);
+      this.traitsObj = [];
+      this.traitType = '';
+      this.emptyData = false;     
+      this.closeStatusModal();
     }
    }
 }
