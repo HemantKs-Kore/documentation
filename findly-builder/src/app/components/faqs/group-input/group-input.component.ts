e1e8ce13318afd9670dc642b516459afb1e02839
vinayReturns <vinay.potaraju@kore.com>
2020-09-08 18:57:38 +0530

    groups hidden

--- findly-builder/src/app/components/faqs/group-input/group-input.component.ts
+++ findly-builder/src/app/components/faqs/group-input/group-input.component.ts
@@ -1,11 +1,14 @@
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import { WorkflowService } from '@kore.services/workflow.service';
 import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
 import {MatChipInputEvent} from '@angular/material/chips';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import { FaqsService } from '../../../services/faqsService/faqs.service';
+import { ServiceInvokerService } from '@kore.services/service-invoker.service';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-group-input',
@@ -20,34 +23,46 @@ export class GroupInputComponent implements OnInit {
   groupCtrl = new FormControl();
   filteredgroups: Observable<string[]>;
   groups: string[] = [];
-  allgroups: string[] = ['Key1', 'Ley1', 'Li2', 'Key34', 'Kiu5'];
-
+  allGroups: string[] = [];
+  selectedApp: any;
+  searchIndexId: any;
+  groupVal: any;
+  allValues: any;
+  currentSugg: any;
   @ViewChild('groupInput') groupInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
-  constructor(private faqService: FaqsService) {
-    this.filteredgroups = this.groupCtrl.valueChanges.pipe(
-        startWith(null),
-        map((group: string | null) => group ? this._filter(group) : this.allgroups.slice()));
+  constructor(  private faqService: FaqsService,
+                private service: ServiceInvokerService,
+                public workflowService: WorkflowService) {
+
   }
 
   ngOnInit(): void {
+    this.selectedApp = this.workflowService.selectedApp();
+    this.searchIndexId = this.selectedApp.searchIndexes[0]._id;
+    this.getAllGroups();
+    setInterval(()=>{
+      // console.log(this.currentSugg);
+    }, 1000)
   }
 
   add(event: MatChipInputEvent): void {
+    if(this.allGroups.indexOf(event.value) == -1) {
+      event.value = '';
+      this.groupCtrl.setValue(null);
+      return;
+    }
     const input = event.input;
     const value = event.value;
-
     // Add our group
     if ((value || '').trim()) {
       this.groups.push(value.trim());
     }
-
     // Reset the input value
     if (input) {
       input.value = '';
     }
-
     this.groupCtrl.setValue(null);
   }
 
@@ -59,15 +74,86 @@ export class GroupInputComponent implements OnInit {
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.groups.push(this.groupInput.nativeElement.value + ":" +event.option.viewValue);
-    this.faqService.groupAdded.next(this.groups);
-    this.groupInput.nativeElement.value = '';
-    this.groupCtrl.setValue(null);
+    // this.groups.push(this.groupInput.nativeElement.value + ":" +event.option.viewValue);
+    // this.faqService.groupAdded.next(this.groups);
+    // this.groupInput.nativeElement.value = '';
+    // this.groupCtrl.setValue(null);
+    let valG = this.groupInput.nativeElement.value;
+    if(valG.indexOf('.') > -1) {
+      let gg = valG.substr(0, valG.indexOf('.'));
+      if(this.allGroups.indexOf(gg) == -1) {
+        valG = '';
+        this.groupCtrl.setValue(null);
+        return;
+      }
+      this.groups.push(gg + ":" +event.option.viewValue);
+      this.faqService.groupAdded.next(this.groups);
+      this.groupCtrl.setValue(null);
+      this.groupInput.nativeElement.value = '';
+      // this.groupCtrl.setValue(null);
+    }
+    else {
+      this.groupCtrl.setValue(event.option.viewValue+".")
+      this.groupInput.nativeElement.value = event.option.viewValue+".";
+      this.currentSugg = this.groupVal[event.option.viewValue];
+    }
+  }
+  
+  getAllGroups() {
+    let params = {
+      searchIndexId: this.searchIndexId,
+      offset: 0,
+      limit: 100
+    };
+    this.service.invoke('get.allGroups', params).subscribe(res => {
+      this.allGroups = _.pluck(res.groups, 'name');
+      this.allValues = _.map(_.pluck(res.groups, 'attributes'), o=>{ return _.pluck(o, 'value')});
+      this.groupVal = _.object(this.allGroups, this.allValues);
+      this.currentSugg = this.allGroups;
+      //commented for now
+      // this.filteredgroups = this.groupCtrl.valueChanges.pipe(
+      //   startWith('g'),
+      //   map((group: string | null) => group ? this._filter(group) : this.allGroups.slice()));
+    }, err=> {} )
+  }
+
+  changeList(event) {
+    if(!event) { return; }
+    if(event.indexOf('.')> -1) {
+      let gg = event.substr(0, event.indexOf('.'));
+      if(this.allGroups.indexOf(gg) == -1) {
+        this.groupInput.nativeElement.value = '';
+        this.groupCtrl.setValue(null);
+        return;
+      }
+      else { 
+        this.currentSugg = this.groupVal[gg]; 
+      }
+    }
+    else if(this.allGroups.indexOf(event) > -1) { 
+      this.currentSugg = [];
+      this.currentSugg = this.groupVal[event]; 
+    }
+    else { 
+      this.currentSugg = this.allGroups; 
+    }
   }
 
   private _filter(value: string): string[] {
+    if(value.indexOf('.')> -1) {
+      let gg = value.substr(0, value.indexOf('.'));
+      if(this.allGroups.indexOf(gg) == -1) {
+        this.groupInput.nativeElement.value = '';
+        this.groupCtrl.setValue(null);
+        return;
+      }
+      else {
+        let vv = value.substr(value.indexOf('.')+1);
+        return this.currentSugg.filter(group => group.toLowerCase().indexOf(vv) === 0);
+      }
+    }
     const filterValue = value.toLowerCase();
-    return this.allgroups.filter(group => group.toLowerCase().indexOf(filterValue) === 0);
+    return this.currentSugg.filter(group => group.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
