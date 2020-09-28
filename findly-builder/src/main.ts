4cadd74f54e7554315c5117b6d4071a9478e9ce4
rajasekhar <rajasekhar.balla@kore.com>
2020-09-28 14:11:11 +0530

    FLY-138 Findly DEV Environment Naming and landing page changes

--- findly-builder/src/main.ts
+++ findly-builder/src/main.ts
@@ -6,9 +6,13 @@ import { environment } from './environments/environment';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/addon/lint/json-lint';
+declare global {
+  interface Window { appConfig: any; }
+}
 if (environment.production) {
   enableProdMode();
 }
 
+window.appConfig=environment;
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
