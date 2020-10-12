1553645b5189c4b6a169cfd44751792b21889a16
i_sriharsha <sri.harsha@kore.com>
2020-10-12 17:05:07 +0530

    weights api intigration

--- findly-builder/src/app/components/weights/weights.component.ts
+++ findly-builder/src/app/components/weights/weights.component.ts
@@ -3,6 +3,12 @@ import { RangeSlider } from '../../helpers/models/range-slider.model';
 import { KRModalComponent } from 'src/app/shared/kr-modal/kr-modal.component';
 import { ConfirmationDialogComponent } from 'src/app/helpers/components/confirmation-dialog/confirmation-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { WorkflowService } from '@kore.services/workflow.service';
+import { ServiceInvokerService } from '@kore.services/service-invoker.service';
+import { NotificationService } from '@kore.services/notification.service';
+import * as _ from 'underscore';
+import { Observable } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 @Component({
   selector: 'app-weights',
   templateUrl: './weights.component.html',
@@ -10,13 +16,28 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class WeightsComponent implements OnInit {
   addEditWeighObj:any= null;
+  loadingContent = true;
   addDditWeightPopRef:any;
   disableCancle:any = true;
+  sliderMin = 0;
+  sliderMax = 10;
+  currentEditIndex: any = -1
+  fields:any ={};
+  searchModel;
   @ViewChild('addDditWeightPop') addDditWeightPop: KRModalComponent;
   constructor(
     public dialog: MatDialog,
+    public workflowService: WorkflowService,
+    private service: ServiceInvokerService,
+    private notificationService: NotificationService,
   ) { }
-  weights:any = [
+  selectedApp: any = {};
+  serachIndexId;
+  queryPipelineId;
+  pipeline;
+  weights:any = []
+  sliderOpen;
+  dummyweights:any = [
     {
      name : 'Proximity',
      description : 'Signifies how near are the matching query words in the records',
@@ -36,81 +57,82 @@ export class WeightsComponent implements OnInit {
       name : 'Last Modified',
       description : 'Signifies number of words from the query matched the title of the results',
       sliderObj :new RangeSlider(0, 10, 1, 2,'titleMatch')
-     },
-     {
-      name : 'Proximity',
-      description : 'Signifies how near are the madwetching query words in the records',
-      sliderObj :new RangeSlider(0, 10, 1, 2,'prdedewwdewoximity')
-     },
-     {
-       name : 'Match Count',
-       description : 'Signifies Number of query words matching exactly',
-       sliderObj :new RangeSlider(0, 10, 1, 2,'exactMadewdewtch')
-      },
-      {
-       name : 'Title Match',
-       description : 'Signifies number of words from the query matched at least once',
-       sliderObj :new RangeSlider(0, 10, 1, 2,'madewtchCodwedunt')
-      },
-      {
-       name : 'Last Modified',
-       description : 'Signifies number of words from the query matched the title of the results',
-       sliderObj :new RangeSlider(0, 10, 1, 2,'titleMdewdatch')
-      },
-      {
-        name : 'Proximity',
-        description : 'Signifies how near are the matching query words in the records',
-        sliderObj :new RangeSlider(0, 10, 1, 2,'proximitdewyd')
-       },
-       {
-         name : 'Match Count',
-         description : 'Signifies Number of query words matching exactly',
-         sliderObj :new RangeSlider(0, 10, 1, 2,'edewxactMadetch')
-        },
-        {
-         name : 'Title Match',
-         description : 'Signifies number of words from the query matched at least once',
-         sliderObj :new RangeSlider(0, 10, 1, 2,'dewmatcdehCount')
-        },
-        {
-         name : 'Last Modified',
-         description : 'Signifies number of words from the query matched the title of the results',
-         sliderObj :new RangeSlider(0, 10, 1, 2,'tdewditledwedMatch')
-        },
-        {
-          name : 'Last Modified',
-          description : 'Signifies number of words from the query matched the title of the results',
-          sliderObj :new RangeSlider(0, 10, 1, 2,'tdewdewditledwedMatch')
-         },
-         {
-          name : 'Last Modified',
-          description : 'Signifies number of words from the query matched the title of the results',
-          sliderObj :new RangeSlider(0, 10, 1, 2,'tdewditledewdwedMatch')
-         },
-         {
-          name : 'Last Modified',
-          description : 'Signifies number of words from the query matched the title of the results',
-          sliderObj :new RangeSlider(0, 10, 1, 2,'tdedewdwditledwedMatch')
-         },
-         {
-          name : 'Last Modified',
-          description : 'Signifies number of words from the query matched the title of the results',
-          sliderObj :new RangeSlider(0, 10, 1, 2,'tddewewditledwedMatch')
-         },
+     }
   ];
   ngOnInit(): void {
+    this.selectedApp = this.workflowService.selectedApp();
+    this.serachIndexId = this.selectedApp.searchIndexes[0]._id;
+    this.queryPipelineId = this.selectedApp.searchIndexes[0].queryPipelineId;
+    this.getWeights();
+    this.getFields();
+  }
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      map(term => term === '' ? []
+        : this.fields.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    )
+
+  formatter = (x: {name: string}) => x.name;
+  selectedField(){
+    this.addEditWeighObj.name = this.searchModel.name;
+  }
+  prepereWeights(){
+    this.weights = [];
+    if(this.pipeline.weights){
+      this.pipeline.weights.forEach((element,i) => {
+         const obj = {
+          name : element.name,
+          description : element.description,
+          sliderObj :new RangeSlider(0, 10, 1, element.value,element.name + i)
+         }
+         this.weights.push(obj);
+      });
+    }
+  }
+  getFields(){
+    const quaryparamats = {
+      searchIndexId : this.serachIndexId,
+   }
+   this.service.invoke('get.groups', quaryparamats).subscribe(
+    res => {
+      this.fields = _.where(res.groups, {action: 'new'});
+    },
+    errRes => {
+    // this.errorToaster(errRes)
+    }
+  );
+  }
+  getWeights(){
+    const quaryparms: any = {
+      searchIndexID:this.serachIndexId,
+      queryPipelineId:this.queryPipelineId,
+    };
+    this.service.invoke('get.queryPipeline', quaryparms).subscribe(res => {
+      this.pipeline=  res.pipeline || {};
+      this.prepereWeights();
+      this.loadingContent = false;
+    }, errRes => {
+      this.loadingContent = false;
+      this.errorToaster(errRes,'Failed to get stop words');
+    });
   }
   valueEvent(val,weight){
-   this.disableCancle = false;
+    if(!this.sliderOpen){
+      this.disableCancle = false;
+    }
    weight.sliderObj.default = val;
   }
-  editWeight(weight){
+  editWeight(weight,index){
     const editWeight = JSON.parse(JSON.stringify(weight));
     editWeight.sliderObj.id = 'editSlider';
     this.addEditWeighObj = editWeight;
+    this.currentEditIndex = index;
     this.openAddEditWeight();
   }
   openAddEditWeight() {
+   this.searchModel = {};
+   this.sliderOpen = true;
    this.addDditWeightPopRef  = this.addDditWeightPop.open();
   }
   openAddNewWeight() {
@@ -124,11 +146,67 @@ export class WeightsComponent implements OnInit {
   closeAddEditWeight() {
    if(this.addDditWeightPopRef && this.addDditWeightPopRef.close){
     this.addDditWeightPopRef.close();
+    this.sliderOpen = false;
+    this.currentEditIndex = -1;
    }
   }
   setDataToActual(){
-    console.log('dataSet');
+    this.prepereWeights();
   }
+  errorToaster (errRes,message) {
+    if (errRes && errRes.error && errRes.error.errors && errRes.error.errors.length && errRes.error.errors[0].msg ) {
+      this.notificationService.notify(errRes.error.errors[0].msg, 'error');
+    } else if (message){
+      this.notificationService.notify(message, 'error');
+    } else {
+      this.notificationService.notify('Somthing went worng', 'error');
+  }
+ }
+ addEditWeight(){
+   const weights = JSON.parse(JSON.stringify(this.weights));
+   if(this.currentEditIndex > -1){
+    weights[this.currentEditIndex ] = this.addEditWeighObj;
+   } else {
+    weights.push(this.addEditWeighObj);
+   }
+   this.addOrUpddate(weights);
+ }
+ getWeightsPayload(weights){
+   const tempweights= [];
+    weights.forEach(weight => {
+     const obj = {
+      name: weight.name,
+      value:weight.sliderObj.default,
+      // description:weight.description,
+     }
+     tempweights.push(obj);
+   });
+   return tempweights
+ }
+ addOrUpddate(weights,dialogRef?) {
+  weights = weights || this.weights;
+  const quaryparms: any = {
+    searchIndexID:this.serachIndexId,
+    queryPipelineId:this.queryPipelineId,
+  };
+  this.pipeline.weights = this.getWeightsPayload(weights);
+  const payload: any = {
+    pipeline:this.pipeline
+  }
+  this.service.invoke('put.queryPipeline', quaryparms, payload).subscribe(res => {
+   this.currentEditIndex = -1;
+   this.pipeline=  res.pipeline || {};
+   this.prepereWeights();
+   this.notificationService.notify('Weight added successfully','success');
+   if(dialogRef && dialogRef.close){
+    dialogRef.close();
+   } else {
+      this.closeAddEditWeight();
+   }
+  }, errRes => {
+    this.errorToaster(errRes,'Failed to add Weight');
+  });
+}
   deleteWeight(record,event,index) {
     if(event){
       event.stopImmediatePropagation();
