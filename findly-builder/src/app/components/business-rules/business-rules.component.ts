1b8a616e8ccce44064365a25a7b73dc267d8143b
devendarba-kore <devendar.bala@kore.com>
2020-11-02 17:30:16 +0530

    business ruls modal styles updated

--- findly-builder/src/app/components/business-rules/business-rules.component.ts
+++ findly-builder/src/app/components/business-rules/business-rules.component.ts
@@ -1,15 +1,23 @@
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-
+import { KRModalComponent } from '../../shared/kr-modal/kr-modal.component';
 @Component({
   selector: 'app-business-rules',
   templateUrl: './business-rules.component.html',
   styleUrls: ['./business-rules.component.scss']
 })
 export class BusinessRulesComponent implements OnInit {
-
+  addBusinessRulesRef:any;
+  @ViewChild('addBusinessRules') addBusinessRules: KRModalComponent;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  openModalPopup(){
+    this.addBusinessRulesRef = this.addBusinessRules.open();
+  }
+  closeModalPopup(){
+    this.addBusinessRulesRef.close();
   }
 
 }
