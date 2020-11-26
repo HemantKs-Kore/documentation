040110346ffe5bf1122bc1700d1b8819bd5fac29
i_sriharsha <sri.harsha@kore.com>
2020-11-26 18:16:05 +0530

    Facets active inactive

--- findly-builder/src/app/components/facets/facets.component.ts
+++ findly-builder/src/app/components/facets/facets.component.ts
@@ -29,6 +29,7 @@ export class FacetsComponent implements OnInit {
       facetName: '',
       facetType: 'value',
       isMultiSelect: false,
+      isFacetActive: true,
       facetValue: {},
     },
     range:{
@@ -255,6 +256,7 @@ export class FacetsComponent implements OnInit {
     } else {
       payload.fieldName = this.selectedField.fieldName;
     }
+      payload.isFacetActive = this.addEditFacetObj.isFacetActive || false;
     this.service.invoke('create.facet', quaryparms,payload).subscribe(res => {
       this.notificationService.notify('Facet created successfully','success');
       this.facets.push(res);
