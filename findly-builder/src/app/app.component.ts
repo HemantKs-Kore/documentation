cfc59863ce524928db7d715b131d3e7694ddc938
i_sriharsha <sri.harsha@kore.com>
2020-11-26 11:10:44 +0530

    Cherrypick

--- findly-builder/src/app/app.component.ts
+++ findly-builder/src/app/app.component.ts
@@ -240,7 +240,7 @@ export class AppComponent implements OnInit , OnDestroy {
     // To modify the web socket url use the following option
     botOptionsFindly.reWriteSocketURL = {
         protocol: 'wss',
-        hostname: 'pilot.findly.ai'
+        hostname:  window.appConfig.API_SERVER_URL.replace('https://','')
     };
     const findlyConfig:any = {
       botOptionsFindly,
