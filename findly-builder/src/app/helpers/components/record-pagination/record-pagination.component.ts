4fe58f2b5b361e3f1e76be6a5f3fab29fc583ab0
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2020-12-04 16:51:31 +0530

    pagintion Dummy

--- findly-builder/src/app/helpers/components/record-pagination/record-pagination.component.ts
+++ findly-builder/src/app/helpers/components/record-pagination/record-pagination.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record-pagination.component.scss']
 })
 export class RecordPaginationComponent implements OnInit {
-
+  testRecord = [];
+  recordPerPage = 10;
+  totalpages = 0;
+  endPage = 0;
+  startpage = 1;
+  inputPage= 1;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    for(let i = 1 ; i < 569 ; i++){
+      this.testRecord.push({i});
+    }
+    this.endPage = Number((this.testRecord.length/this.recordPerPage).toFixed());
+    if(this.inputPage > this.endPage){
+      this.inputPage = this.inputPage - 1;
+    }
 
+  }
+  inputPageChange(){
+
+  }
+  arrowFirst(){
+    this.inputPage = this.startpage;
+  }
+  arrowLast(){
+    this.inputPage = this.endPage;
+  }
+  arrowPrevious(){
+    if(this.startpage > this.inputPage ) this.inputPage = this.inputPage - 1;
+  }
+  arrowNext(){
+    if(this.endPage < this.inputPage ) this.inputPage = this.inputPage + 1;
+  }
 }
