92d13f9e27912be2140a7d2bd9211607d13e91ba
KI0613\Maneeshkumar.M <maneeshkumar.mannam@kore.com>
2021-01-06 12:26:21 +0530

    Adding slides updated in StructuredData landing page.

--- findly-builder/src/app/components/structured-data/structured-data.component.ts
+++ findly-builder/src/app/components/structured-data/structured-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { KRModalComponent } from 'src/app/shared/kr-modal/kr-modal.component';
 
 @Component({
   selector: 'app-structured-data',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuredDataComponent implements OnInit {
 
+  addStructuredDataModalPopRef : any;
+  selectedSourceType: any;
+  availableSources = [
+    {
+      name: 'Import Structured Data',
+      description: 'Import from JSON or CSV',
+      icon: 'assets/icons/content/database-Import.svg',
+      id: 'contentStucturedDataImport',
+      sourceType: 'json',
+      resourceType: 'structuredData'
+    },
+    {
+      name: 'Import Structured Data',
+      description: 'Add structured data manually',
+      icon: 'assets/icons/content/database-add.svg',
+      id: 'contentStucturedDataAdd',
+      sourceType: 'json',
+      resourceType: 'structuredDataManual'
+    }
+  ]
+
+  @ViewChild('addStructuredDataModalPop') addStructuredDataModalPop: KRModalComponent;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openAddStructuredData(key){
+    this.selectedSourceType = this.availableSources.find((s) =>{ if(s.resourceType === key){ return s}});
+    console.log("this.selectedSourceType", this.selectedSourceType);
+    this.addStructuredDataModalPopRef = this.addStructuredDataModalPop.open();
+  }
+
+  cancleSourceAddition() {
+    this.selectedSourceType = null;
+    this.closeStructuredDataModal();
+  }
+
+  closeStructuredDataModal(){
+    if (this.addStructuredDataModalPopRef && this.addStructuredDataModalPopRef.close) {
+      this.addStructuredDataModalPopRef.close();
+    }
   }
 
 }
