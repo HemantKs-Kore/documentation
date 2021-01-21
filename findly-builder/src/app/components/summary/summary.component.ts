b6a58cf0db8463bdd23b79edeee6a4b27f967fdc
RushivarTa-Kore <rushivar.takhur@kore.com>
2021-01-21 14:07:48 +0530

    FLY-1259 bug fixed

--- findly-builder/src/app/components/summary/summary.component.ts
+++ findly-builder/src/app/components/summary/summary.component.ts
@@ -97,6 +97,7 @@ export class SummaryComponent implements OnInit {
       title: 'Summary',
       toShowWidgetNavigation: this.workflowService.showAppCreationHeader()
     };
+    this.current_month = this.listMonths[this.date.getMonth()];
     this.selectedApp = this.workflowService.selectedApp();
     this.serachIndexId = this.selectedApp.searchIndexes[0]._id;
     this.headerService.toggle(toogleObj);
