ee1391ae7a0f93ce4e8a1d4ff61e72e43c140536
pavan ichapurapu <pavan.ichapurapu@kore.com>
2021-04-20 15:09:17 +0530

    usage log code change

--- findly-builder/src/app/components/usage-log/usage-log.component.ts
+++ findly-builder/src/app/components/usage-log/usage-log.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usage-log.component.scss']
 })
 export class UsageLogComponent implements OnInit {
-
+  showSearch;
+  searchUsageLog = '';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  toggleSearch() {
+    if (this.showSearch && this.searchUsageLog) {
+      this.searchUsageLog = '';
+    }
+    this.showSearch = !this.showSearch
+  }
 }
