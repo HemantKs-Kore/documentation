9d2b2d031b5a2b22e6b12c3be0a2d09ade6c8a42
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2021-04-27 22:46:05 +0530

    enviornment[''] Inline Manual

--- findly-builder/src/app/services/inline-manual.service.ts
+++ findly-builder/src/app/services/inline-manual.service.ts
@@ -79,9 +79,9 @@ export class InlineManualService {
         };
         el.src = script;
     }
-
-    if (environment.hasOwnProperty('INLINE_MANUAL_SITE_KEY') && environment['INLINE_MANUAL_SITE_KEY']) {
-        loadInlineManualScripts(environment.INLINE_MANUAL_SITE_KEY);
+    const inline : any = environment;
+    if (inline.hasOwnProperty('INLINE_MANUAL_SITE_KEY') && inline['INLINE_MANUAL_SITE_KEY']) {
+        loadInlineManualScripts(inline['INLINE_MANUAL_SITE_KEY']);
     }
   };
 
