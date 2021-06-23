d018e3abb201d5d36dbc86186b9442788039e3f3
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2021-06-23 21:25:44 +0530

    Prcing Graph

--- findly-builder/src/environments/environment.ts
+++ findly-builder/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   tag: 'dev',
-  API_SERVER_URL: 'https://dev.findly.ai',
+  API_SERVER_URL: 'https://app.findly.ai',
   USE_SESSION_STORE: false
   // API_SERVER_URL: 'http://192.168.10.101:5000'
 };
