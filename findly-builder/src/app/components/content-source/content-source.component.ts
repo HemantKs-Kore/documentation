91d50ce52551d80c300adc12c3a8ba1ca2812241
Havanika sai <havanika.sai@kore.com>
2021-06-30 15:39:43 +0530

    content i18n

--- findly-builder/src/app/components/content-source/content-source.component.ts
+++ findly-builder/src/app/components/content-source/content-source.component.ts
@@ -431,12 +431,12 @@ export class ContentSourceComponent implements OnInit, OnDestroy {
             }
           }
         }
-        res.forEach(element => {
-          if(element.status ! = this.resourcesStatusObj[element._id].status){
-            // this.getJobDetails(source._id)
-            this.getSourceList();
-          }
-        });
+        // res.forEach(element => {
+        //   if(element.status ! = this.resourcesStatusObj[element._id].status){
+        //     // this.getJobDetails(source._id)
+        //     this.getSourceList();
+        //   }
+        // });
         //this.resourcesStatusObj.forEach(element => {
           // if(this.resourcesStatusObj[element._id].status != res[source._id].status){
           //   this.getJobDetails(source._id)
