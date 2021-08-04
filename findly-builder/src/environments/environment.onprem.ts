3ef1be009dca65a29b362183f8cdb09bceaee93f
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2021-08-04 12:53:40 +0530

    Inline Commit again

--- findly-builder/src/environments/environment.onprem.ts
+++ findly-builder/src/environments/environment.onprem.ts
@@ -6,7 +6,8 @@ export const environment = {
     production: true,
     tag:"onprem",
     API_SERVER_URL:'https://onprem.kore.ai/',
-    USE_SESSION_STORE:false
+    USE_SESSION_STORE:false,
+    INLINE_MANUAL_SITE_KEY :"1ec224ee46620656a9b18a17c80587a3"
   };
   
   /*
--- findly-builder/src/environments/environment.pilot.ts
+++ findly-builder/src/environments/environment.pilot.ts
@@ -6,7 +6,8 @@ export const environment = {
     production: true,
     tag:"pilot",
     API_SERVER_URL:'https://pilot-bots.kore.ai',
-    USE_SESSION_STORE:false
+    USE_SESSION_STORE:false,
+    INLINE_MANUAL_SITE_KEY :"1ec224ee46620656a9b18a17c80587a3"
   };
   
   /*
