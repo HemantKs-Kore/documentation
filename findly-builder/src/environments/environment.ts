ec51b677f93a736624676745b417a81796c61845
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2021-08-04 12:48:21 +0530

    Inline Commit again

--- findly-builder/src/environments/environment.ts
+++ findly-builder/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   tag: 'dev',
   API_SERVER_URL: 'https://app.findly.ai',
   USE_SESSION_STORE: false,
-  INLINE_MANUAL_SITE_KEY: "1ec224ee46620656a9b18a17c80587a3"
+  INLINE_MANUAL_SITE_KEY: "1ec224ee46620656a9b18a17c80587a3",
   // API_SERVER_URL: 'http://192.168.10.101:5000'
 };
 
