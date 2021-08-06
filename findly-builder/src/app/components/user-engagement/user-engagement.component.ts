78732b881cbcb8e40f3e4010301d49d8938f8856
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2021-08-06 12:17:29 +0530

    FLY - 3008

--- findly-builder/src/app/components/user-engagement/user-engagement.component.ts
+++ findly-builder/src/app/components/user-engagement/user-engagement.component.ts
@@ -116,7 +116,7 @@ export class UserEngagementComponent implements OnInit {
   lowValue = 0;
   isyAxismostUsedBrowserdata = false;
   isyAxisGeodata = false;
-  startDate: any = moment().subtract({ days: 7 });
+  startDate: any = moment().subtract({ days: 6 });
   endDate: any = moment();
   minDate: any = moment().subtract({ days: 95 });
   maxDate: any = moment();
