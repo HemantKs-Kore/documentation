7114e9d3a7b2587c25cf2771af3faa94efeef901
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2021-09-08 19:11:25 +0530

    Moving code in Cloud Test for mixpanel

--- findly-builder/src/environments/environment.clouddev.ts
+++ findly-builder/src/environments/environment.clouddev.ts
@@ -3,6 +3,7 @@ export const environment = {
   tag: 'dev',
   API_SERVER_URL: 'https://dev.findly.ai',
   USE_SESSION_STORE: false,
+  MIXPANEL_KEY : "b8cca4172b544f41f2dde97189667d81",
   INLINE_MANUAL_SITE_KEY :"1ec224ee46620656a9b18a17c80587a3"
   // API_SERVER_URL: 'http://192.168.10.101:5000'
 };
