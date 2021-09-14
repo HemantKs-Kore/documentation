a3fcdfa5d7e1d35ed2963d8eb1756071af7947d1
Sunil Radheyshyam Singh <sunil.singh@kore.com>
2021-09-14 11:30:48 +0530

    Mixpanel only in QA

--- findly-builder/src/environments/environment.ts
+++ findly-builder/src/environments/environment.ts
@@ -7,7 +7,6 @@ export const environment = {
   tag: 'dev',
   API_SERVER_URL: 'https://pilot.searchassist.ai',
   USE_SESSION_STORE: false,
-  MIXPANEL_KEY : "b8cca4172b544f41f2dde97189667d81",
   INLINE_MANUAL_SITE_KEY: "1ec224ee46620656a9b18a17c80587a3",
   // API_SERVER_URL: 'http://192.168.10.101:5000'
 };
