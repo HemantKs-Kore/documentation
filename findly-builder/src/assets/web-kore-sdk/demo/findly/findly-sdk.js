238431ed34b16797bef72b15b9a74d55d778f3cb
pavan ichapurapu <pavan.ichapurapu@kore.com>
2021-10-04 19:40:35 +0530

    flickering  searched results ui issue fixed and websocket timeout issue fixed

--- findly-builder/src/assets/web-kore-sdk/demo/findly/findly-sdk.js
+++ findly-builder/src/assets/web-kore-sdk/demo/findly/findly-sdk.js
@@ -5306,7 +5306,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
                     popularSearches: _self.vars.searchObject.popularSearches.slice(0, 5),
                     showSearches: searchConfigurationCopy ? searchConfigurationCopy.showSearches : 'recent'
                   });
-                  if (_self.vars.searchObject.recents.length ) {
+                  if (_self.vars.searchObject.recents.length && !_self.vars.enterIsClicked && !window.isBotLocked) {
                     $('.search-body').css('display', 'block');
                     $('.search-body').removeClass('hide');
                     $('.livesearchResultsNotFound').css('display', 'none');
@@ -5930,8 +5930,11 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
                               $('.search-body').css('display', 'none');
                               $('.search-body').addClass('hide');
                             } else {
-                              $('.search-body').css('display', 'block');
-                              $('.search-body').removeClass('hide');
+                              if(!_self.vars.enterIsClicked && !window.isBotLocked){
+                                $('.search-body').css('display', 'block');
+                                $('.search-body').removeClass('hide');
+                              }
+                             
                             }
                           } else if ((!$('body').hasClass('top-down') && !$('.bottom-up-search').val())) {
                             $('.search-body').css('display', 'none');
@@ -6079,8 +6082,10 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
                               _self.pubSub.publish('sa-search-result', { ...dataObj, ...{ isLiveSearch: false, isFullResults: true, selectedFacet: _self.vars.selectedFacetFromSearch | 'all results' } });
 
                             } else {
-                              $('.search-body').css('display', 'block');
-                              $('.search-body').removeClass('hide');
+                              if(!_self.vars.enterIsClicked && !window.isBotLocked){
+                                $('.search-body').css('display', 'block');
+                                $('.search-body').removeClass('hide');
+                              }
                               _self.pubSub.publish('sa-search-result', dataObj);
                               _self.pubSub.publish('sa-source-type', _self.getFacetsAsArray(facets));
                             }
@@ -6132,6 +6137,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
         })
 
         $(dataHTML).off('focus', '#search').on('focus', '#search', function (e) {
+          _self.vars.enterIsClicked = false;
           if (!_self.vars.isSocketInitialize) {
             _self.bot.init(_self.vars.botConfig, _self.config.messageHistoryLimit);
             _self.bindSocketEvents();
@@ -6195,7 +6201,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
               //   popularSearches: _self.vars.searchObject.popularSearches.slice(0, 6)
               // });
               // $('.search-body').html(freqData);
-              if (!$('body').hasClass('top-down')) {
+              if (!$('body').hasClass('top-down') && !_self.vars.enterIsClicked && !($('body').hasClass('top-down') ? $('.search-top-down').val() : $('.bottom-up-search').val())) {
                 _self.pubSub.publish('sa-generate-recent-search');
               }
             }
@@ -6403,7 +6409,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
         $('.search-body').addClass('h-100');
         $('.search-body').append(creditCard);
         $('.search-body').removeClass('hide');
-        $('#searchChatContainer').addClass('bgfocus');
+        // $('#searchChatContainer').addClass('bgfocus');
         $('.search-body').scrollTop(0);
         _self.bindPerfectScroll(creditCard, '.card-container', null, 'x', 'creditcard');
 
@@ -7012,7 +7018,9 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
           // if ((!$('body').hasClass('top-down') && $('.bottom-up-search').is(':visible')) || ($('body').hasClass('top-down') && $('#frequently-searched-box').is(':visible'))) {
           //   return;
           // }
-          _self.pubSub.publish('sa-generate-recent-search');
+          if(!_self.vars.enterIsClicked && !($('body').hasClass('top-down') ? $('.search-top-down').val() : $('.bottom-up-search').val())){
+            _self.pubSub.publish('sa-generate-recent-search');
+          }
         }, 150);
       }
 
@@ -7038,7 +7046,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
           if (searchConfigurationCopy && searchConfigurationCopy.showSearchesEnabled) {
             if (!window.isBotLocked) {
               $('.search-body').removeClass('hide');
-              $('#searchChatContainer').addClass('bgfocus');
+              // $('#searchChatContainer').addClass('bgfocus');
             }
           }
           $('.suggestion-search-data-parent').css('display', 'block');
@@ -7503,7 +7511,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
       $('.searchBox').addClass('hide');
       $('.search-body').append(userLogin);
       $('.search-body').removeClass('hide');
-      $('#searchChatContainer').addClass('bgfocus');
+      // $('#searchChatContainer').addClass('bgfocus');
     };
     FindlySDK.prototype.getFrequentlySearched = function (url, type, payload) {
       var bearer = "bearer " + this.bot.options.accessToken || this.API.jstBarrer || "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.wrUCyDpNEwAaf4aU5Jf2-0ajbiwmTU3Yf7ST8yFJdqM";
@@ -8877,6 +8885,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
       clearTimeout(_disconnectBotTimer);
       _self.vars.isSocketInitialize = false;
       _self.vars.isSocketReInitialize = false;
+      _self.bot.initialized = false;
       $('.typingIndicatorContent').css('display', 'none');
     }
     FindlySDK.prototype.resetSocketDisconnection = function () {
@@ -9138,24 +9147,36 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
       console.log("Message to Bot", messageToBot);
       $('.typingIndicatorContent').css('display', 'block');
-      var sendMsgTimeOut = _self.vars.isSocketReInitialize ? 4000 : 0
-      _self.vars.isSocketReInitialize = false;
-      setTimeout(() => {
-        _self.bot.sendMessage(messageToBot, function messageSent(err) {
-          if (err && err.message) {
-            setTimeout(function () {
-              $('#msg_' + clientMessageId).find('.messageBubble').append('<div class="errorMsg">Send Failed. Please resend.</div>');
-            }, 350);
-          }
-        });
-      }, sendMsgTimeOut)
+      // var sendMsgTimeOut = _self.vars.isSocketReInitialize ? 4000 : 0
+      // _self.vars.isSocketReInitialize = false;
+      _self.checkWbInitialized(messageToBot,clientMessageId);
+     
       // if (_self.isDev == false) {
       _self.resetPingMessage();
       _self.resetSocketDisconnection();
       // }
 
     };
-
+    FindlySDK.prototype.checkWbInitialized = function (messageToBot, clientMessageId) {
+      var _self = this;
+      var sendMsgTimeOut = _self.vars.isSocketReInitialize ? 1000 : 0;
+      $('.typingIndicatorContent').css('display', 'block');
+      setTimeout(() => {
+        if (_self.bot.initialized) {
+          _self.vars.isSocketReInitialize = false;
+          _self.bot.sendMessage(messageToBot, function messageSent(err) {
+            if (err && err.message) {
+              setTimeout(function () {
+                $('#msg_' + clientMessageId).find('.messageBubble').append('<div class="errorMsg">Send Failed. Please resend.</div>');
+              }, 350);
+            }
+          });
+        } else {
+          _self.vars.isSocketReInitialize = true;
+          _self.checkWbInitialized(messageToBot, clientMessageId);
+        }
+      }, sendMsgTimeOut)
+    }
     FindlySDK.prototype.getTemplate = function (type) {
       var menuTemplate = '<script id="chat_message_tmpl" type="text/x-jqury-tmpl">\
         <div class="menuItemCntr">\
