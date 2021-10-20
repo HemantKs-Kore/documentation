0ea8ca59cf69b172f2490b5e43615b134c79351a
RushivarTa-Kore <rushivar.takhur@kore.com>
2021-10-20 12:49:25 +0530

    get apps sorting issue fixed

--- findly-builder/src/app/components/apps-home/apps-home.ts
+++ findly-builder/src/app/components/apps-home/apps-home.ts
@@ -132,8 +132,8 @@ export class AppsListingComponent implements OnInit {
   showBoarding: boolean = true;
   public getAllApps() {
     this.service.invoke('get.apps').subscribe(res => {
-      this.prepareApps(res);
       if (res && res.length) {
+        this.prepareApps(res);
         this.workflowService.showAppCreationHeader(false);
         this.selectedAppType('All');
         this.sortApp('Created Date');
@@ -147,7 +147,7 @@ export class AppsListingComponent implements OnInit {
         //   this.inlineManual.visited('CREATE_APP')
         // }
         this.showBoarding = true;
-        this.openBoradingJourney()
+        this.openBoradingJourney();
       }
     }, errRes => {
       console.log(errRes);
