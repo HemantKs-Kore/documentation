755cd11a3aa243056cb313aa248c1bad29c44d4e
KI0649\Havanika.Sai <havanika.sai@kore.com>
2021-11-22 11:07:53 +0530

    FLY-3597,3623,3624,Select All in SD(implemented basic fun to show the option)

--- findly-builder/src/app/components/faq-source/faq-source.component.ts
+++ findly-builder/src/app/components/faq-source/faq-source.component.ts
@@ -297,6 +297,7 @@ export class FaqSourceComponent implements OnInit, AfterViewInit, OnDestroy {
     this.closeAddsourceModal();
     // this.closeStatusModal()
     this.showSourceAddition = null;
+    this.getSourceList();
   }
   closeSourcePopupEvent() {
     this.closeAddsourceModal();
