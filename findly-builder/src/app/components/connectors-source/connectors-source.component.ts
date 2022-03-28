bb3b7c646e9cac47e13f2f10e4648a4cd9ec1ff1
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-03-28 11:50:23 +0530

    created connector source component

---
+++ findly-builder/src/app/components/connectors-source/connectors-source.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connectors-source',
+  templateUrl: './connectors-source.component.html',
+  styleUrls: ['./connectors-source.component.scss']
+})
+export class ConnectorsSourceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
