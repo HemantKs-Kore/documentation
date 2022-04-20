764bc5e1cfd9e8175c4a4248b420ec41d7236f7e
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-04-20 11:35:37 +0530

    FLY-4820 spinner added for simualte

--- findly-builder/src/app/components/index/index.component.ts
+++ findly-builder/src/app/components/index/index.component.ts
@@ -66,6 +66,7 @@ export class IndexComponent implements OnInit, OnDestroy, AfterViewInit
   pollingSubscriber: any = null;
   submitted: boolean = false;
   showNewStageType: boolean = false;
+  loadingSimulate:boolean = true;
   subscription: Subscription;
   @ViewChild('tleft') public tooltip: NgbTooltip;
   @ViewChild('addFieldModalPop') addFieldModalPop: KRModalComponent;
@@ -1717,7 +1718,8 @@ export class IndexComponent implements OnInit, OnDestroy, AfterViewInit
     }
   }
   simulate(warningmessage?)
-  {
+  {     
+    this.loadingSimulate=true;
     let plainScriptTxt: any;
     if (this.newMappingObj && this.newMappingObj.custom_script &&
       this.newMappingObj.custom_script.defaultValue && this.newMappingObj.custom_script.defaultValue.script)
@@ -1774,7 +1776,8 @@ export class IndexComponent implements OnInit, OnDestroy, AfterViewInit
           //   this.newMappingObj.custom_script.defaultValue.script && 
           //   this.newMappingObj.custom_script.defaultValue.script != plainScriptTxt) {
           //   this.newMappingObj.custom_script.defaultValue.script = plainScriptTxt;
-          // }
+          // } 
+            this.loadingSimulate=false;          
         this.simulteObj.simulating = false;
         this.addcode(res);
         this.mixpanel.postEvent('Initiated Simulator', {});
@@ -1791,6 +1794,7 @@ export class IndexComponent implements OnInit, OnDestroy, AfterViewInit
         this.simulteObj.simulationInprogress = false;
         
       }, errRes => {
+        this.loadingSimulate = false;
         if(warningmessage){
           this.notificationService.notify(warningmessage,'warning');
         }
