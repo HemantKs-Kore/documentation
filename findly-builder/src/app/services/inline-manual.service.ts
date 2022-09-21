f818e5c25b6a60dadf8c6868d7c20f4e3566b870
KI0604\Sunil.Singh <sunil.singh@kore.com>
2022-09-21 12:41:39 +0530

    Appcues Code

--- findly-builder/src/app/services/inline-manual.service.ts
+++ findly-builder/src/app/services/inline-manual.service.ts
@@ -211,24 +211,7 @@ export class InlineManualService {
         window.Appcues.identify(
             koreUID, // unique, required
             {
-          
-              // recommended (optional) properties
-          
-            //   createdAt: 1566932390, // Unix timestamp of user signup date
-            //   purchasedAd: 1566932395, // Unix timestamp of account purchase date (leave null if empty)
-            //   planTier: "Standard", // Current user’s plan tier
-            //   role: "Admin", // Current user’s role or permissions
-            //   accountId: "1234", // Current user's account ID
-            //   firstName: "John", // current user's first name
-          
-              // additional suggestions
-          
-              //companyName: "Acme Corp", // Current user’s company name
               email: koreEmail, // Current user's email
-            //   location: "90210", // a zipcode, state, or country enables location-based targeting
-            //   version: "2.0", // users on different versions may need to see different content
-            //   language: "spanish", // for multi-language applications
-            //   renewalDate: 1577880288 // to remind users to renew
             }
           );
       }    
