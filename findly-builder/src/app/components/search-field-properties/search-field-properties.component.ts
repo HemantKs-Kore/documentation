191b66b7012caad594bbd1a1ae5b2a58ed20c712
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-09-22 12:00:56 +0530

    added search field propeties component

---
+++ findly-builder/src/app/components/search-field-properties/search-field-properties.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-field-properties',
+  templateUrl: './search-field-properties.component.html',
+  styleUrls: ['./search-field-properties.component.scss']
+})
+export class SearchFieldPropertiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
