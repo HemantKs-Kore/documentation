173cdfeaa3fd79e3235dbf50bbd1d48c4826f507
RushivarTa-Kore <rushivar.takhur@kore.com>
2022-09-28 22:43:40 +0530

    FLY-5965, FLY-5955, FLY-5940, FLY-5935 issues fixed

--- findly-builder/src/app/components/content-source/content-source.component.ts
+++ findly-builder/src/app/components/content-source/content-source.component.ts
@@ -257,7 +257,7 @@ export class ContentSourceComponent implements OnInit, OnDestroy {
   allowURLArray: Array<Object> = [{ condition: 'contains', url: '' }];
   blockURLArray: Array<Object> = [{ condition: 'contains', url: '' }];
   authorizationFieldObj: any = { type: '', key: '', value: '', isEnabled: true, isShow: false, isEditable: false, duplicateObj: { type: '', key: '', value: '' } };
-  formFieldObj: any = { type: '', key: '', value: '', isEnabled: true, isShow: false, isEditable: false, duplicateObj: { type: '', key: '', value: '' } };
+  formFieldObj: any = { type: '', key: '', value: '', isEnabled: true, isShow: false, isEditable: false, isRequired: true,  duplicateObj: { type: '', key: '', value: '' } };
   autorizationFieldTypes: Array<String> = ['header', 'payload', 'querystring', 'pathparam'];
   testTypes: Array<String> = ['text_presence', 'redirection_to', 'status_code'];
   authenticationTypes: Array<String> = ['basic', 'form'];
@@ -1652,28 +1652,6 @@ export class ContentSourceComponent implements OnInit, OnDestroy {
       }
     });
   }
-  urlCondition(condition, type) {
-    type == 'allow' ? this.urlConditionAllow = condition : this.urlConditionBlock = condition;
-    type == 'allow' ? this.allowUrl.condition = condition : this.blockUrl.condition = condition;
-    if (condition === 'is') {
-      type === 'allow' ? this.allowUrl.name = "Equals to" : this.blockUrl.name = "Equals to";
-    }
-    else if (condition === 'isNot') {
-      type === 'allow' ? this.allowUrl.name = "Not equals to" : this.blockUrl.name = "Not equals to";
-    }
-    else if (condition === 'beginsWith') {
-      type === 'allow' ? this.allowUrl.name = "Begins with" : this.blockUrl.name = "Begins with";
-    }
-    else if (condition === 'endsWith') {
-      type === 'allow' ? this.allowUrl.name = "Ends with" : this.blockUrl.name = "Ends with";
-    }
-    else if (condition === 'contains') {
-      type === 'allow' ? this.allowUrl.name = "Contains" : this.blockUrl.name = "Contains";
-    }
-    else if (condition === 'doesNotContains') {
-      type === 'allow' ? this.allowUrl.name = "Doesn't contain" : this.blockUrl.name = "Doesn't contain";
-    }
-  }
   scheduleData(scheduleData) {
     //if (this.selectedSource?.advanceSettings?.scheduleOpts) this.selectedSource['advanceSettings'].scheduleOpts = scheduleData;
     if(this.editSource?.advanceOpts?.scheduleOpts) this.editSource.advanceOpts.scheduleOpts = scheduleData;
@@ -2046,14 +2024,14 @@ export class ContentSourceComponent implements OnInit, OnDestroy {
       const count = this.countValidationInputs(array);
       if (count === array.length) {
         this.editSource.authorizationProfle.formFields.push(this.formFieldObj);
-        this.formFieldObj = { type: '', key: '', value: '', isEnabled: true, isShow: false, isEditable: false, duplicateObj: { type: '', key: '', value: '' } };
+        this.formFieldObj = { type: '', key: '', value: '', isEnabled: true,isRequired: true,  isShow: false, isEditable: false, duplicateObj: { type: '', key: '', value: '' } };
       }
       else {
         this.notificationService.notify('Enter the required fields to proceed', 'error');
       }
     }
     else if (type === 'cancel') {
-      this.formFieldObj = { type: '', key: '', value: '', isEnabled: true, isShow: false, isEditable: false, duplicateObj: { type: '', key: '', value: '' } };
+      this.formFieldObj = { type: '', key: '', value: '', isEnabled: true, isShow: false,isRequired: true, isEditable: false, duplicateObj: { type: '', key: '', value: '' } };
     }
   }
 
