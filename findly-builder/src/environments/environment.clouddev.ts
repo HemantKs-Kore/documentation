bdab3217b35777c415ccd19d787551ffe5332354
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-11-01 11:38:37 +0530

    pricing enable disable testing in app instance

--- findly-builder/src/environments/environment.clouddev.ts
+++ findly-builder/src/environments/environment.clouddev.ts
@@ -4,8 +4,8 @@ export const environment = {
   API_SERVER_URL: 'https://dev.findly.ai',
   USE_SESSION_STORE: false,
   INLINE_MANUAL_SITE_KEY :"1ec224ee46620656a9b18a17c80587a3",
-  PROD_FLAG:true,
-  Installer_FLAG:true,
+  PROD_FLAG:false,
+  Installer_FLAG:false,
   APPCUES:{
     ENABLE : true,
     APPCUES_KEY : '112004'
