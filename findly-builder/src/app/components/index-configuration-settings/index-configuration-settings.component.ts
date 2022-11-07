a5ea0df1d5306cc58eac57411d27ebf6de912c73
SaiTejaUd-Kore <saiteja.udutha@kore.com>
2022-11-07 12:52:57 +0530

    FLY-6345 - Index Filed UI Design Support

--- findly-builder/src/app/components/index-configuration-settings/index-configuration-settings.component.ts
+++ findly-builder/src/app/components/index-configuration-settings/index-configuration-settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { KRModalComponent } from 'src/app/shared/kr-modal/kr-modal.component';
+import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
 
 @Component({
   selector: 'app-index-configuration-settings',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexConfigurationSettingsComponent implements OnInit {
 
+  addLangModalPopRef: any;
+
+
+  @ViewChild('addLangModalPop') addLangModalPop: KRModalComponent;
+  @ViewChild('perfectScroll') perfectScroll: PerfectScrollbarComponent;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openModalPopup() {
+    this.addLangModalPopRef = this.addLangModalPop.open();
+    setTimeout(() => {
+      this.perfectScroll.directiveRef.update();
+      this.perfectScroll.directiveRef.scrollToTop();
+    }, 500)
+  }
+
+  closeModalPopup() {
+    this.addLangModalPopRef.close();
   }
 
 }
