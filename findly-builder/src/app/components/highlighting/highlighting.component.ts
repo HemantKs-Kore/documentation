892b2d8eae4b95b1cb77e29fb6435339d0752450
SaiTejaUd-Kore <saiteja.udutha@kore.com>
2022-11-09 19:54:51 +0530

    ui created for presentable highlighting spell corrections

--- findly-builder/src/app/components/highlighting/highlighting.component.ts
+++ findly-builder/src/app/components/highlighting/highlighting.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { KRModalComponent } from 'src/app/shared/kr-modal/kr-modal.component';
+import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
 
 @Component({
   selector: 'app-highlighting',
@@ -9,7 +11,24 @@ export class HighlightingComponent implements OnInit {
 
   constructor() { }
 
+  highlightAppearanceModalPopRef: any;
+
+  @ViewChild('highlightAppearanceModalPop') highlightAppearanceModalPop: KRModalComponent;
+  @ViewChild('perfectScroll') perfectScroll: PerfectScrollbarComponent;
+
   ngOnInit(): void {
+  }
+
+  openModalPopup() {
+    this.highlightAppearanceModalPopRef = this.highlightAppearanceModalPop.open();
+    setTimeout(() => {
+      this.perfectScroll.directiveRef.update();
+      this.perfectScroll.directiveRef.scrollToTop();
+    }, 500)
+  }
+
+  closeModalPopup() {
+    this.highlightAppearanceModalPopRef.close();
   }
 
 }
