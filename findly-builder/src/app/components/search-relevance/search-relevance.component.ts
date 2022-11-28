6ffce8de9d86a2996f7cd92e5e325d92b7b17e95
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-11-28 19:39:21 +0530

    added condition for beta icon and tried to add the slider in the custom configuration

--- findly-builder/src/app/components/search-relevance/search-relevance.component.ts
+++ findly-builder/src/app/components/search-relevance/search-relevance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RangeSlider } from '../../helpers/models/range-slider.model';
 
 @Component({
   selector: 'app-search-relevance',
@@ -8,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class SearchRelevanceComponent implements OnInit {
 
   constructor() { }
-
+  sliderOpen;
+  disableCancle: any = true;
   ngOnInit(): void {
+    this.prepereSliderObj(1, 2)
   }
+
+  prepereSliderObj(index, scale?) {
+    return new RangeSlider(0, 5, 1, scale || 3, 'outcomeScale' + index)
+  }
+  
+
+  valueEvent(val, outcomeObj) {
+    outcomeObj.scale = val;
+  }
+
+
+
+   addOrUpddate(weight, dialogRef?, type?)
+   {}
 
 }
