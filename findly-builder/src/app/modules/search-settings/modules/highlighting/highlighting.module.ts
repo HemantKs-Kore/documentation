5932ab77c1290b43d19533043507481d4079304a
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-11-28 18:28:52 +0530

    Lazy loading for highlight presentable and spell correct

--- findly-builder/src/app/modules/search-settings/modules/highlighting/highlighting.module.ts
+++ findly-builder/src/app/modules/search-settings/modules/highlighting/highlighting.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HighlightingRoutingModule } from './highlighting-routing.module';
 import { HighlightingComponent } from './highlighting.component';
+import { FindlySharedModule } from 'src/app/modules/findly-shared/findly-shared.module';
 
 
 @NgModule({
   declarations: [HighlightingComponent],
   imports: [
     CommonModule,
-    HighlightingRoutingModule
+    HighlightingRoutingModule,
+    FindlySharedModule
   ]
 })
 export class HighlightingModule { }
--- findly-builder/src/app/modules/search-settings/modules/presentable/presentable.module.ts
+++ findly-builder/src/app/modules/search-settings/modules/presentable/presentable.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { PresentableRoutingModule } from './presentable-routing.module';
 import { PresentableComponent } from './presentable.component';
+import { FindlySharedModule } from 'src/app/modules/findly-shared/findly-shared.module';
+// import { TranslateService } from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [PresentableComponent],
   imports: [
     CommonModule,
-    PresentableRoutingModule
+    PresentableRoutingModule,
+    FindlySharedModule
   ]
 })
-export class PresentableModule { }
+export class PresentableModule { 
+  // constructor(private translate: TranslateService) {}
+}
