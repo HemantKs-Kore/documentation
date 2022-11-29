d4628c7c7d68a2297f652f6c61633199c155d6bd
Hemant <hemant.singh@kore.com>
2022-11-29 17:29:55 +0530

    route fix

--- findly-builder/src/app/modules/search-settings/search-settings.component.ts
+++ findly-builder/src/app/modules/search-settings/search-settings.component.ts
@@ -6,20 +6,21 @@ import { of, interval, Subject, Subscription } from 'rxjs';
 import { PerfectScrollbarComponent, PerfectScrollbarDirective } from 'ngx-perfect-scrollbar';
 import { NotificationService } from '@kore.services/notification.service';
 import { ServiceInvokerService } from '@kore.services/service-invoker.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-settings',
   templateUrl: './search-settings.component.html',
-  styleUrls: ['./search-settings.component.scss']
+  styleUrls: ['./search-settings.component.scss'],
 })
 export class SearchSettingsComponent implements OnInit {
   selectedApp;
   indexPipelineId;
   streamId: any;
   queryPipelineId: any;
-  querySubscription : Subscription;
+  querySubscription: Subscription;
   serachIndexId;
-  pipeline:any=[];
+  pipeline: any = [];
   selectedComponent: String = 'weights';
   componentsArray: Array<Object> = [
     { key: 'Weights', value: 'weights' },
@@ -32,39 +33,55 @@ export class SearchSettingsComponent implements OnInit {
     { key: 'Stop Words', value: 'stop_words' },
     { key: 'Bot Actions', value: 'bot_actions' },
     { key: 'Small Talk', value: 'small_talk' },
-    { key: 'Custom Configurations', value: 'custom_configurations' }
+    { key: 'Custom Configurations', value: 'custom_configurations' },
   ];
   constructor(
     public workflowService: WorkflowService,
     private appSelectionService: AppSelectionService,
     private notificationService: NotificationService,
     private service: ServiceInvokerService,
-  ) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.selectedApp = this.workflowService.selectedApp();
     this.serachIndexId = this.selectedApp.searchIndexes[0]._id;
     this.indexPipelineId = this.workflowService.selectedIndexPipeline();
-    this.queryPipelineId = this.workflowService.selectedQueryPipeline() ? this.workflowService.selectedQueryPipeline()._id : '';
+    this.queryPipelineId = this.workflowService.selectedQueryPipeline()
+      ? this.workflowService.selectedQueryPipeline()._id
+      : '';
     this.getQuerypipeline();
-    this.querySubscription = this.appSelectionService.queryConfigSelected.subscribe(res => {
-      this.indexPipelineId = this.workflowService.selectedIndexPipeline();
-      this.queryPipelineId = this.workflowService.selectedQueryPipeline() ? this.workflowService.selectedQueryPipeline()._id : ''
-      this.getQuerypipeline();
-   })
+    this.querySubscription =
+      this.appSelectionService.queryConfigSelected.subscribe((res) => {
+        this.indexPipelineId = this.workflowService.selectedIndexPipeline();
+        this.queryPipelineId = this.workflowService.selectedQueryPipeline()
+          ? this.workflowService.selectedQueryPipeline()._id
+          : '';
+        this.getQuerypipeline();
+      });
   }
   //** get Query pipeline API call */
-  getQuerypipeline(){
+  getQuerypipeline() {
     const quaryparms: any = {
       searchIndexID: this.serachIndexId,
       queryPipelineId: this.queryPipelineId,
-      indexPipelineId: this.indexPipelineId
+      indexPipelineId: this.indexPipelineId,
     };
-    this.service.invoke('get.queryPipeline', quaryparms).subscribe(res => {
-      this.pipeline = res;
-    },errRes => {
-      this.notificationService.notify('failed to get querypipeline details', 'error')
-    });
+    this.service.invoke('get.queryPipeline', quaryparms).subscribe(
+      (res) => {
+        this.pipeline = res;
+      },
+      (errRes) => {
+        this.notificationService.notify(
+          'failed to get querypipeline details',
+          'error'
+        );
+      }
+    );
+  }
+
+  openSettingsScreen(value) {
+    this.router.navigate(['search-settings', value]);
   }
 
   ngOnDestroy() {
