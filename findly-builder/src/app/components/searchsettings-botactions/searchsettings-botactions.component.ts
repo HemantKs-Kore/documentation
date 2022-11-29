c3960802eba6e00f7244d510ec8d2da75731edaa
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-11-29 13:40:09 +0530

    created seperate components for bot-actions, custom configurations and snippets and moved html and css

---
+++ findly-builder/src/app/components/searchsettings-botactions/searchsettings-botactions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchsettings-botactions',
+  templateUrl: './searchsettings-botactions.component.html',
+  styleUrls: ['./searchsettings-botactions.component.scss']
+})
+export class SearchsettingsBotactionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
