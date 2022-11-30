70ecfbf96899b2284c7e726b4a2e4e779235a3bb
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-11-30 16:33:26 +0530

    botactions in search settings lazy module completed

--- findly-builder/src/app/modules/search-settings/modules/bot-actions/bot-actions.module.ts
+++ findly-builder/src/app/modules/search-settings/modules/bot-actions/bot-actions.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { BotActionsRoutingModule } from './bot-actions-routing.module';
 import { BotActionsComponent } from './bot-actions.component';
+import { FindlySharedModule } from 'src/app/modules/findly-shared/findly-shared.module';
+
 
 
 @NgModule({
   declarations: [BotActionsComponent],
   imports: [
     CommonModule,
-    BotActionsRoutingModule
+    BotActionsRoutingModule,
+    FindlySharedModule
   ]
 })
 export class BotActionsModule { }
