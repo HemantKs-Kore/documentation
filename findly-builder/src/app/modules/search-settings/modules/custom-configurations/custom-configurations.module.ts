78368f1dddce93564823aaadeb60af6bf14407c9
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-11-30 16:57:52 +0530

    custom configuration  design changes for lazy module

--- findly-builder/src/app/modules/search-settings/modules/custom-configurations/custom-configurations.module.ts
+++ findly-builder/src/app/modules/search-settings/modules/custom-configurations/custom-configurations.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CustomConfigurationsRoutingModule } from './custom-configurations-routing.module';
 import { CustomConfigurationsComponent } from './custom-configurations.component';
+import { FindlySharedModule } from 'src/app/modules/findly-shared/findly-shared.module';
 
 
 @NgModule({
   declarations: [CustomConfigurationsComponent],
   imports: [
     CommonModule,
-    CustomConfigurationsRoutingModule
+    CustomConfigurationsRoutingModule,
+    FindlySharedModule
   ]
 })
 export class CustomConfigurationsModule { }
