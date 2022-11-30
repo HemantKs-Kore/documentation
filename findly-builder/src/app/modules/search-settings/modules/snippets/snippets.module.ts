84a09f5f84cc70011cd61709ce8624e680707ef9
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-11-30 16:41:10 +0530

    snippets designchanges added in shared module for lazy loading

--- findly-builder/src/app/modules/search-settings/modules/snippets/snippets.module.ts
+++ findly-builder/src/app/modules/search-settings/modules/snippets/snippets.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SnippetsRoutingModule } from './snippets-routing.module';
 import { SnippetsComponent } from './snippets.component';
+import { FindlySharedModule } from 'src/app/modules/findly-shared/findly-shared.module';
 
 
 @NgModule({
   declarations: [SnippetsComponent],
   imports: [
     CommonModule,
-    SnippetsRoutingModule
+    SnippetsRoutingModule,
+    FindlySharedModule
   ]
 })
 export class SnippetsModule { }
