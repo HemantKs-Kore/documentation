0acfa6760053017370f345df21e82e4b811bb9ae
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-12-01 16:09:13 +0530

    Threshold slider get and post api added for search relevance

--- findly-builder/src/app/components/search-relevance/search-relevance.component.ts
+++ findly-builder/src/app/components/search-relevance/search-relevance.component.ts
@@ -6,47 +6,107 @@ import { ServiceInvokerService } from '@kore.services/service-invoker.service';
 import { of, interval, Subject, Subscription } from 'rxjs';
 import { RangeSlider } from '../../helpers/models/range-slider.model';
 
+
 @Component({
   selector: 'app-search-relevance',
   templateUrl: './search-relevance.component.html',
   styleUrls: ['./search-relevance.component.scss']
 })
 export class SearchRelevanceComponent implements OnInit {
-  @Input() searchrelevancedata: any;
+  searchrelevancedata: any;
+  selectedApp;
+  indexPipelineId;
+  streamId: any;
+  serachIndexId
+  queryPipelineId;
+  querySubscription : Subscription;
   weightModal = {
+      "fieldName": "",
+      "fieldDataType": "",
+      "fieldId": "",
+      "sliderObj": {
+          "minVal": 0,
+          "maxVal": 100,
+          "step": 1,
+          "default": 0,
+          "id": "matchThreshold0",
+          "key": "",
+          "enable": true
+      }
   };
  
-  constructor() { }
+  constructor(
+    public workflowService: WorkflowService,
+    private appSelectionService: AppSelectionService,
+    private notificationService: NotificationService,
+    private service: ServiceInvokerService,
+  ) { }
   sliderOpen;
   disableCancle: any = true;
   ngOnInit(): void {
-    console.log(this.searchrelevancedata.matchThreshold)
+    this.selectedApp = this.workflowService.selectedApp();
+    this.serachIndexId = this.selectedApp.searchIndexes[0]._id;
+    this.indexPipelineId = this.workflowService.selectedIndexPipeline();
+    this.queryPipelineId = this.workflowService.selectedQueryPipeline() ? this.workflowService.selectedQueryPipeline()._id : '';
     this.prepareThreshold()
-
+    this.querySubscription = this.appSelectionService.queryConfigSelected.subscribe(res => {
+      this.indexPipelineId = this.workflowService.selectedIndexPipeline();
+      this.queryPipelineId = this.workflowService.selectedQueryPipeline() ? this.workflowService.selectedQueryPipeline()._id : ''
+      this.prepareThreshold()
+    })   
+    
+    
   }
 
   prepareThreshold(){ 
-  
-          const name = ('matchThreshold' || '').replace(/[^\w]/gi, '')
-          const obj = {
-            fieldName: name,
-            fieldDataType: 'number',
-            fieldId: 'matchThreshold',
-            sliderObj: new RangeSlider(0, 100, 1,this.searchrelevancedata.matchThreshold,'matchThreshold','',true)
-          }
-          this.weightModal= obj;
+    const quaryparms: any = {
+      searchIndexID: this.serachIndexId,
+      queryPipelineId: this.queryPipelineId,
+      indexPipelineId: this.indexPipelineId
+    };
+    this.service.invoke('get.queryPipeline', quaryparms).subscribe(res => {
+      this.searchrelevancedata = res.settings.searchRelevance;
+      const name = ('matchThreshold' || '').replace(/[^\w]/gi, '')
+      const obj = {
+        fieldName: name,
+        fieldDataType: 'number',
+        fieldId: 'matchThreshold',
+        sliderObj: new RangeSlider(0, 100, 1,this.searchrelevancedata.matchThreshold,'matchThreshold0','',true)
+      }
+      this.weightModal= obj;
+      
+    },errRes => {
+      this.notificationService.notify('failed to get querypipeline details', 'error')
+    }); 
   }
  
  
   
 
   valueEvent(val, outcomeObj) {
-    outcomeObj.scale = val;
+    const quaryparms:any={
+      indexPipelineId:this.workflowService.selectedIndexPipeline(),
+      queryPipelineId:this.workflowService.selectedQueryPipeline() ? this.workflowService.selectedQueryPipeline()._id : '',
+      searchIndexId:this.serachIndexId
+    }
+      const payload:any={
+        settings: {
+          searchRelevance: {
+            matchThreshold: val
+        }
+      }    
+    }
+    this.service.invoke('put.queryPipeline', quaryparms,payload).subscribe(res => {
+      this.prepareThreshold()
+
+      //this.notificationService.notify("updated successfully",'success');
+    }, errRes => {
+      this.notificationService.notify("Failed to update",'error');
+    });
+  }
+  ngOnDestroy() {
+    this.querySubscription ? this.querySubscription.unsubscribe() : false;
   }
 
-
-
-   addOrUpddate(weight, dialogRef?, type?)
-   {}
 
 }
