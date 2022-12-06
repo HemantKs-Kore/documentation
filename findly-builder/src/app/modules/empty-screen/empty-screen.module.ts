0f9637dd585dbe8dd967396a67cfd36c86defedd
Hemant <hemant.singh@kore.com>
2022-12-06 16:43:35 +0530

    empty screen module cleanup

--- findly-builder/src/app/modules/empty-screen/empty-screen.module.ts
+++ findly-builder/src/app/modules/empty-screen/empty-screen.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmptyScreenComponent } from './empty-screen.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [EmptyScreenComponent],
-  imports: [CommonModule, MatProgressSpinnerModule, TranslateModule],
+  imports: [CommonModule],
   exports: [EmptyScreenComponent],
 })
 export class EmptyScreenModule {}
