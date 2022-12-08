6d019d9a9c6239793cdcec2313979a187242b1e7
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2022-12-08 17:09:20 +0530

    custom configuration changes are completed

--- findly-builder/src/app/modules/search-settings/modules/custom-configurations/custom-configurations.component.ts
+++ findly-builder/src/app/modules/search-settings/modules/custom-configurations/custom-configurations.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,Input,EventEmitter } from '@angular/core';
+import { WorkflowService } from '@kore.services/workflow.service';
+import { AppSelectionService } from '@kore.services/app.selection.service';
+import { NotificationService } from '@kore.services/notification.service';
+import { ServiceInvokerService } from '@kore.services/service-invoker.service';
+import { of, interval, Subject, Subscription } from 'rxjs';
+import {v4 } from 'uuid';
 @Component({
   selector: 'app-custom-configurations',
   templateUrl: './custom-configurations.component.html',
@@ -7,9 +12,144 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomConfigurationsComponent implements OnInit {
 
-  constructor() { }
+  formData = {};
+  selectedApp;
+  indexPipelineId;
+  streamId: any;
+  serachIndexId
+  queryPipelineId
+  querySubscription : Subscription;
+  customConfig: any =[]
+
+  constructor(
+    public workflowService: WorkflowService,
+    private appSelectionService: AppSelectionService,
+    private notificationService: NotificationService,
+    private service: ServiceInvokerService,
+  ) { }
 
   ngOnInit(): void {
+    this.selectedApp = this.workflowService.selectedApp();
+    this.serachIndexId = this.selectedApp.searchIndexes[0]._id;
+    this.indexPipelineId = this.workflowService.selectedIndexPipeline();
+    this.queryPipelineId = this.workflowService.selectedQueryPipeline() ? this.workflowService.selectedQueryPipeline()._id : '';
+    this.getcustomConfigList()
+    this.querySubscription = this.appSelectionService.queryConfigSelected.subscribe(res => {
+      this.indexPipelineId = this.workflowService.selectedIndexPipeline();
+      this.queryPipelineId = this.workflowService.selectedQueryPipeline() ? this.workflowService.selectedQueryPipeline()._id : ''   
+      this.getcustomConfigList()   
+  })
+ 
+
+  }
+  getcustomConfigList(){
+    this.getQuerypipeline()
+  }
+  //** get querypipeline */
+getQuerypipeline() {
+  const quaryparms: any = {
+    searchIndexID: this.serachIndexId,
+    queryPipelineId: this.queryPipelineId,
+    indexPipelineId: this.indexPipelineId,
+  };
+  this.service.invoke('get.queryPipeline', quaryparms).subscribe(
+    (res) => {
+       this.customConfig = res.settings.customConfiguration.values;
+      // this.customConfig = [
+      //   {
+      //     key: 'English', value: 'en',_id:'01'
+      //   },
+      //   {
+      //     key: 'Japanies', value: 'jp',_id:'02'
+      //   },
+      //   {
+      //     key: 'Chinies', value: 'cn',_id:'03'
+      //   },
+      //   {
+      //     key: 'Hindi', value: 'in',_id:'04'
+      //   }
+      // ]
+    },
+    (errRes) => {
+      this.notificationService.notify(
+        'failed to get querypipeline details',
+        'error'
+      );
+    }
+  );
+}
+  //**Spupdate query pipeline */
+  sendData(configObj) {
+    const quaryparms: any = {
+      indexPipelineId: this.workflowService.selectedIndexPipeline(),
+      queryPipelineId: this.workflowService.selectedQueryPipeline()
+        ? this.workflowService.selectedQueryPipeline()._id
+        : '',
+      searchIndexId: this.serachIndexId,
+    };
+    var payload: any = {
+      settings: {
+        customConfiguration: {
+          values: configObj
+        },
+      },
+    };
+    this.service.invoke('put.queryPipeline', quaryparms, payload).subscribe(
+      (res) => {
+        
+        //this.notificationService.notify('updated successfully', 'success');
+      },
+      (errRes) => {
+        this.notificationService.notify('Failed to update', 'error');
+      }
+    );
   }
 
+
+deleteCustomConfig(data) {
+  this.customConfig = this.customConfig.filter(item => item !== data);
+  this.sendData(this.customConfig)
+  this.notificationService.notify('deleted succesfully', 'success');
+  this.formData = {};
+}
+
+  updateCustomConfig(data) {
+    this.formData = data;
+  }
+
+  updateCustomConfigValue(data) {
+    console.log('ss', data);
+      this.customConfig = this.customConfig.map(item => {
+        if (item._id === data._id) {
+          return data;
+        }
+
+        return item;
+      })
+      this.sendData(this.customConfig)
+      this.notificationService.notify('Updated successfully', 'success');
+  }
+
+  onFormSubmit(formData) {
+    if (formData._id) {
+      // Update
+      this.updateCustomConfigValue(formData)
+    } else {
+      // Add
+      const isKeyExists = this.customConfig.find(item => item.key === formData.key);
+
+      if (isKeyExists) {
+        this.notificationService.notify('duplicate key found try using unique key', 'error');
+      }
+      else{
+        this.customConfig = [{_id: v4(), ...formData}, ...this.customConfig];
+        this.sendData(this.customConfig)
+        this.notificationService.notify('Added successfully', 'success');
+      }
+    }
+  }
+
+  openCustomConfig() {
+    this.formData = {};
+  }
 }
--- findly-builder/src/app/modules/search-settings/modules/custom-configurations/custom-configurations.module.ts
+++ findly-builder/src/app/modules/search-settings/modules/custom-configurations/custom-configurations.module.ts
@@ -4,14 +4,18 @@ import { CommonModule } from '@angular/common';
 import { CustomConfigurationsRoutingModule } from './custom-configurations-routing.module';
 import { CustomConfigurationsComponent } from './custom-configurations.component';
 import { FindlySharedModule } from 'src/app/modules/findly-shared/findly-shared.module';
+import { CustomConfigFormComponent } from './components/custom-config-form/custom-config-form.component';
+import { CustomConfigListComponent } from './components/custom-config-list/custom-config-list.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [CustomConfigurationsComponent],
+  declarations: [CustomConfigurationsComponent, CustomConfigFormComponent, CustomConfigListComponent],
   imports: [
     CommonModule,
     CustomConfigurationsRoutingModule,
-    FindlySharedModule
+    FindlySharedModule,
+    FormsModule
   ]
 })
 export class CustomConfigurationsModule { }
