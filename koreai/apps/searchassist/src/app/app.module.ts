7380a00509550158b5d808008c6a9170317d5f43
Hemant <hemant.singh@kore.com>
2023-01-12 11:53:57 +0530

    i18n setup root

--- koreai/apps/searchassist/src/app/app.module.ts
+++ koreai/apps/searchassist/src/app/app.module.ts
@@ -1,11 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/home/', '.json');
+}
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+      extend: true,
+    })],
   providers: [],
   bootstrap: [AppComponent],
 })
