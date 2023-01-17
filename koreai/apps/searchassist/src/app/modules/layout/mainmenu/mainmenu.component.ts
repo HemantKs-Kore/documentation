b4ff5f9248bae4b9a82837f6efb885a72627d1b4
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2023-01-17 12:55:49 +0530

    angular upgrade seperated the header body and main menu seperate modules

---
+++ koreai/apps/searchassist/src/app/modules/layout/mainmenu/mainmenu.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainmenu',
+  templateUrl: './mainmenu.component.html',
+  styleUrls: ['./mainmenu.component.scss'],
+})
+export class MainmenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
