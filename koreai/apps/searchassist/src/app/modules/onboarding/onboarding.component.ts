fa290a2ac1a14f2540c71256871351c458a1e995
Hemant <hemant.singh@kore.com>
2023-01-27 11:36:55 +0530

    onboarding module

---
+++ koreai/apps/searchassist/src/app/modules/onboarding/onboarding.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding',
+  templateUrl: './onboarding.component.html',
+  styleUrls: ['./onboarding.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OnboardingComponent {}
