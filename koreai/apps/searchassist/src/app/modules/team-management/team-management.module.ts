196a2daf3e1560464185283b0aa322749d8eee22
Hemant <hemant.singh@kore.com>
2023-01-31 20:09:53 +0530

    latest changes

--- koreai/apps/searchassist/src/app/modules/team-management/team-management.module.ts
+++ koreai/apps/searchassist/src/app/modules/team-management/team-management.module.ts
@@ -4,13 +4,24 @@ import { CommonModule } from '@angular/common';
 import { TeamManagementRoutingModule } from './team-management-routing.module';
 import { TeamManagementComponent } from './team-management.component';
 import { KrModalModule } from '../../shared/kr-modal/kr-modal.module';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { MatChipsModule } from '@angular/material/chips';
 import { SharedPipesModule } from '@kore.apps/helpers/filters/shared-pipes.module';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(
+    http,
+    './assets/i18n/team-management/',
+    '.json'
+  );
+}
 
 @NgModule({
   declarations: [TeamManagementComponent],
@@ -18,7 +29,14 @@ import { SharedPipesModule } from '@kore.apps/helpers/filters/shared-pipes.modul
     CommonModule,
     TeamManagementRoutingModule,
     KrModalModule,
-    TranslateModule,
+    TranslateModule.forChild({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
     MatFormFieldModule,
     FormsModule,
     NgbTypeaheadModule,
