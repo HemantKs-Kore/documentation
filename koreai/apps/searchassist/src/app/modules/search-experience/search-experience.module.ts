600343af28200e62173aaea3cc441aed1cb84a77
Hemant <hemant.singh@kore.com>
2023-02-09 14:37:29 +0530

    picker module added

--- koreai/apps/searchassist/src/app/modules/search-experience/search-experience.module.ts
+++ koreai/apps/searchassist/src/app/modules/search-experience/search-experience.module.ts
@@ -6,9 +6,13 @@ import { SearchExperienceComponent } from './search-experience.component';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { KrModalModule } from '../../shared/kr-modal/kr-modal.module';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbDropdownModule,
+  NgbTooltipModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { UseronboardingJourneyModule } from '@kore.apps/helpers/components/useronboarding-journey/useronboarding-journey.module';
 import { PickerModule } from '@ctrl/ngx-emoji-mart';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   declarations: [SearchExperienceComponent],
@@ -21,6 +25,8 @@ import { PickerModule } from '@ctrl/ngx-emoji-mart';
     UseronboardingJourneyModule,
     NgbTooltipModule,
     PickerModule,
+    NgbDropdownModule,
+    ColorPickerModule,
   ],
 })
 export class SearchExperienceModule {}
