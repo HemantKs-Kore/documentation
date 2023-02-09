b2883cd9b1321c369c6ae01c67404f5486bc7dee
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2023-02-09 11:00:08 +0530

    search experience console issue fixed

--- koreai/apps/searchassist/src/app/modules/search-experience/search-experience.component.ts
+++ koreai/apps/searchassist/src/app/modules/search-experience/search-experience.component.ts
@@ -181,8 +181,8 @@ export class SearchExperienceComponent implements OnInit, OnDestroy {
   }
   //validate max length in textarea
   testInputLength(event) {
-    if (event.value.length > 150) {
-      event.value = event.value.substring(0, 150);
+    if (event?.value?.length > 150) {
+      event.value = event?.value?.substring(0, 150);
     }
   }
   //select tab on number
