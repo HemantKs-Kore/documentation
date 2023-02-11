299976c2b667cd23ccbabbffab4e1056ad9d9be5
Hemant <hemant.singh@kore.com>
2023-02-11 18:37:42 +0530

    rangy import

--- koreai/tools/build.js
+++ koreai/tools/build.js
@@ -1,14 +1,25 @@
-const shell = require('shelljs');
+// const shell = require('shelljs');
 
-function buildRangy() {
-  if (
-    shell.exec(
-      `uglifyjs node_modules/rangy/lib/rangy-core.js --module --compress --mangle --warn --output apps/searchassist/src/assets/js/rangy.min.js`
-    )
-  ) {
-    shell.echo('Rangy build success');
-    shell.exit(1);
+// function buildRangy() {
+//   if (
+//     shell.exec(
+//       `uglifyjs node_modules/rangy/lib/rangy-core.js --module --compress --mangle --warn --output apps/searchassist/src/assets/js/rangy.min.js`
+//     )
+//   ) {
+//     shell.echo('Rangy build success');
+//     shell.exit(1);
+//   }
+// }
+
+// buildRangy();
+
+var concat = require('concat-files');
+concat(
+  ['node_modules/rangy/lib/rangy-core.js'],
+  './apps/searchassist/src/assets/js/rangy.min.js',
+  function (err) {
+    if (err) throw err;
+    console.log('done');
+    // buildRangy();
   }
-}
-
-buildRangy();
+);
