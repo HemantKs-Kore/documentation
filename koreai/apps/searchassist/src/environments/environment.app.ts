18399395283d0710e8a38cf586bc278177978e7e
Hemant <hemant.singh@kore.com>
2023-02-16 17:21:41 +0530

    env config update

--- koreai/apps/searchassist/src/environments/environment.app.ts
+++ koreai/apps/searchassist/src/environments/environment.app.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   tag: 'app',
-  API_SERVER_URL: 'https://searchassist-qa.kore.ai',
+  API_SERVER_URL: 'https://searchassist-app.kore.ai',
   USE_SESSION_STORE: false,
   INLINE_MANUAL_SITE_KEY: '1ec224ee46620656a9b18a17c80587a3',
   deployment_type: '',
