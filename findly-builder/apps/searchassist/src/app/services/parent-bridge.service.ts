3c50b7071b08f72362f697793644149c6b727e62
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2023-03-07 17:47:59 +0530

    Added the topic guide mix panel events

--- findly-builder/apps/searchassist/src/app/services/parent-bridge.service.ts
+++ findly-builder/apps/searchassist/src/app/services/parent-bridge.service.ts
@@ -22,27 +22,39 @@ export class ParentBridgeService {
       messageEvent,
       (e) => {
         if (e.data && e.data.action) {
-          const data: any = e.data;
+          const data = e.data;
           this.parentEvent.next(data);
+          //console.log("TOpic Guide ---",data);
           if (data?.payload?.url && data?.action == 'videoModal') {
             this.mixpanel?.postEvent('Topic - Play Topic Video', {
               'Video File URL': data?.payload?.url,
-              'Topic Name': data?.payload?.topicName,
+              'Topic Name': data?.payload?.topicName
+                ? data?.payload?.topicName
+                : data?.payload?.title,
             });
+            //console.log("video",data?.payload?.topicName)
+            console.log('Topic - Play Topic Video');
           } else if (data?.payload?.faqName && data?.action == 'faq') {
             this.mixpanel?.postEvent('Topic - FAQ Accessed', {
               'FAQ String': data?.payload?.faqName,
-              'Topic Name': data?.payload?.topicName,
+              'Topic Name': data?.payload?.topicName
+                ? data?.payload?.topicName
+                : data?.payload?.path?.split('/').pop(),
             });
+            console.log('Topic - FAQ Accessed');
           } else if (data?.payload?.docUrl && data?.action == 'Documentation') {
             this.mixpanel?.postEvent('Topic - Documentation visited', {
-              'Topic Name': data?.payload?.topicName,
+              'Topic Name': data?.payload?.topicName
+                ? data?.payload?.topicName
+                : data?.payload?.path?.split('/').pop(),
             });
+            console.log('Topic - Documentation visited');
           } else if (
             data?.frameStatus == 'loaded' &&
             data?.action == 'guidedTour'
           ) {
             this.mixpanel?.postEvent('Topic - Enter Topic Guide', {});
+            console.log('Topic - Enter Topic Guide');
           }
         }
       },
